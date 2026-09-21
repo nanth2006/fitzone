819c50d3ef084f8a7fc7a31154cfb432c8ae642a
nanth2006 <nanthakumar2006geetha02@gmail.com>
2026-09-21 22:10:19 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,14 @@ const startServer = async () => {
   app.use('/api/user', userRoutes);
   app.use('/api/workouts', workoutRoutes);
 
-  app.get('/', (req, res) =>  res.send("server running"),
+  app.get('/', (req, res) => 
   res.json({ message: 'FitZone Gym API running smoothly' }));
 
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => 
   
   console.log(`Server running on port ${PORT}`));
-  res.send("server running")
+ 
 };
 
 startServer();
